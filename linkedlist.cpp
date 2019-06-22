c1dc19f2bbbfe0cd053260353286986b679cc92c
Ashish S <ashishjsharda@gmail.com>
2019-06-22 14:53:20 -0400

    linkedlist.cpp
    linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+        int main()
+        {
+        list<int> gqlist1;
+        gqlist1.push_back(10);
+        gqlist1.push_back(20);
+        cout<<gqlist1.front()<<"\n";
+        cout<<gqlist1.back();
+        return 0;
+        }
